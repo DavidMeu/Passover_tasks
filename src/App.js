37a3a548e8deb8b95dd4f3102417f2044028c39e
DavidM <dmeushar@marvell.com>
2020-04-05 13:04:03 +0300

    Initial commit for passover tasks
    Change-Id: I18916a6073723b150d744e7be169bc9916979f15

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './Todos'
+import AddTodo from './AddTodo'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    todos: [
+      { id: 1, content: 'House cleaning' },
+      { id: 2, content: 'New teeth brushes'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+  }
+
+  addTodo = (todo) => {
+      todo.id = Math.random()
+      let todos = [...this.state.todos, todo];
+      this.setState({
+        todos //when key and value have the same name.
+      })
+  }
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Passover Tasks</h1>
+        <h2 className="center blue-text">Once done click to delete</h2>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
